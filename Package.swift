3a86e4521411c29d009273ae824859327c44d26c
Jared LaSante <jaredlasante@gmail.com>
2021-06-07 10:51:50 -0400

    Added Package support

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "UIImageViewAlignedSwift",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(name: "UIImageViewAlignedSwift", targets: ["UIImageViewAlignedSwift"])
+    ],
+    targets: [
+        .target(
+            name: "UIImageViewAlignedSwift",
+            path: "UIImageViewAlignedSwift"
+        )
+     ]
+)
